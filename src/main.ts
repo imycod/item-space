ba882c14fd53ef5ec3fba2c8c566658198309a8b
Jay Wu <jay.wu@item.com>
2024-04-24 14:42:30 +0800

    feat：初始化item路由配置

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {createApp} from 'vue';
 import App from './App.vue';
-import router from './router';
+import router from '@/router/index.js';
 import pinia from "@/stores"
 import { i18n } from '@/i18n';
 import PrimeVue from 'primevue/config';
---
+++ src/views/item/Dashboard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import AppConfig from "@/layout/AppConfig.vue";
+</script>
+
+<template>
+  <button class="bg-purple-500">button</button>
+  <Button>button</Button>
+  <app-config></app-config>
+</template>
+
+<style scoped lang="scss">
+
+</style>
