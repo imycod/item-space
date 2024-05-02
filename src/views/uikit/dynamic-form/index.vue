7af3ada3f675afead62cba29bb72204d7b88f608
Jay Wu <jay.wu@item.com>
2024-05-02 22:43:49 +0800

    fix：动态表单

---
+++ src/views/uikit/dynamic-form/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import DynamicFormItem from "@/components/form-layout/DynamicFormItem.vue";
+
+import root from "./model.ts";
+</script>
+
+<template>
+  <div class="mb-5">请输入 test1</div>
+  <DynamicFormItem :form-state="root"></DynamicFormItem>
+</template>
+
+<style scoped lang="scss">
+
+</style>
