a9bd65aa0ab1b575d5260a823c50f55320eb5db6
Jay Wu <jay.wu@item.com>
2024-08-11 16:10:57 +0800

    feat:eventEmitter off

--- src/utils/eventEmitter.ts
+++ src/utils/eventEmitter.ts
@@ -1,3 +1,5 @@
+import {onMounted, onScopeDispose} from "vue"
+
 const eventNames = ['API:UN_AUTHORIZED', 'API:VALIDATION_ERROR', 'STYLE:REMOVE_CUSTOM_STYLE'] as const;
 
 type EventName = typeof eventNames[number];
@@ -16,6 +18,21 @@ class EventEmitter {
 	emit(eventName: EventName, ...args: any[]) {
 		this.listeners[eventName]?.forEach((listener) => listener(...args));
 	}
+
+	off(eventName: EventName, listener: () => void) {
+		this.listeners[eventName].delete(listener);
+	}
 }
 
-export default new EventEmitter();
+const eventEmitter = new EventEmitter();
+export function useEventListener(eventName: EventName, listener: () => void) {
+	onMounted(() => {
+		eventEmitter.on(eventName, listener);
+	});
+
+	onScopeDispose(() => {
+		eventEmitter.off(eventName, listener);
+	});
+}
+
+export default eventEmitter;
